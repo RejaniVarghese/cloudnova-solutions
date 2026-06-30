0894e8707e7aabd1cd9e96a104ef8d76cad4af51
RejaniVarghese <rejanimv@yahoo.co.in>
2026-06-30 15:21:32 +0000

    Improve application status page

--- status.php
+++ status.php
@@ -1,34 +1,62 @@
-<?php include 'includes/header.php'; ?>
+<?php
+date_default_timezone_set('Asia/Kolkata');
+include 'includes/header.php';
+?>
 
 <section>
 
-<h2>System Status</h2>
+<h2>Application Status Dashboard</h2>
 
-<table border="1" cellpadding="10" cellspacing="0">
+<table border="1" cellpadding="10" cellspacing="0" width="100%">
 
 <tr>
-<th>Service</th>
+<th>Component</th>
 <th>Status</th>
 </tr>
 
 <tr>
-<td>AWS EC2</td>
+<td>AWS EC2 Instance</td>
 <td style="color:green;">🟢 Running</td>
 </tr>
 
 <tr>
-<td>Docker</td>
+<td>Docker Container</td>
 <td style="color:green;">🟢 Running</td>
 </tr>
 
 <tr>
-<td>Jenkins</td>
-<td style="color:green;">🟢 Running</td>
+<td>Jenkins CI/CD</td>
+<td style="color:green;">🟢 Operational</td>
 </tr>
 
 <tr>
 <td>PHP Application</td>
 <td style="color:green;">🟢 Healthy</td>
+</tr>
+
+<tr>
+<td>Web Server</td>
+<td style="color:green;">🟢 Apache Running</td>
+</tr>
+
+<tr>
+<td>Deployment</td>
+<td>Docker on AWS EC2</td>
+</tr>
+
+<tr>
+<td>Version</td>
+<td>1.0.0</td>
+</tr>
+
+<tr>
+<td>Environment</td>
+<td>Production</td>
+</tr>
+
+<tr>
+<td>Last Updated</td>
+<td><?php echo date("d-m-Y H:i:s"); ?></td>
 </tr>
 
 </table>
